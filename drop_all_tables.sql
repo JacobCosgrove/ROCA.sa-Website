f0ff1b7edeed445cfb1e518214fae77cbad8aa25
Lenus Jack <Lenusjack4@yahoo.com>
2020-03-10 16:02:05 -0500

    Create drop_all_tables.sql
    use before creating tables again

---
+++ drop_all_tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS associations;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS job_openings;
+DROP TABLE IF EXISTS applicants;
+DROP TABLE IF EXISTS search_committees;
